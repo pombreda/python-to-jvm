a9aed0f2597ad57d2bb52631da4805e1d486119d
Simeon Visser <simeon87@gmail.com>
2012-03-30 23:22:21 +0200

    Add start of Java byte code generation

--- java/codegenerator.py
+++ java/codegenerator.py
@@ -1,3 +1,7 @@
+import binascii
 
 def generate_byte_code(data, filename):
-    return []
+    with open(filename, 'wb') as f:
+        f.write(binascii.unhexlify("CAFEBABE"))
+        f.write(binascii.unhexlify("0000"))
+        f.write(binascii.unhexlify("0032")) # Java 6
