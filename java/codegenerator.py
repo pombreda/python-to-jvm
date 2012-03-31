e45d349f54215c70adc34a79dc45d475916a99ea
Simeon Visser <simeon87@gmail.com>
2012-03-31 23:04:33 +0200

    Added first part of byte code generation

--- java/codegenerator.py
+++ java/codegenerator.py
@@ -1,7 +1,23 @@
-import binascii
+from binascii import unhexlify
+
+def write_CONSTANT_Utf8(s):
+    attribute_type = unhexlify("01")
+    attribute_length = unhexlify(hex(len(s))[2:].rjust(4, '0'))
+    attribute_data = bytes(s.encode())
+    return attribute_type + attribute_length + attribute_data
+
+def write_constant_pool(data, f):
+    pass
+
+def write_class_data(data, pool, f):
+    pass
 
 def generate_byte_code(data, filename):
     with open(filename, 'wb') as f:
-        f.write(binascii.unhexlify("CAFEBABE"))
-        f.write(binascii.unhexlify("0000"))
-        f.write(binascii.unhexlify("0032")) # Java 6
+        f.write(unhexlify("CAFEBABE"))
+        f.write(unhexlify("0000"))
+        f.write(unhexlify("0032")) # Java 6
+        pool = write_constant_pool(data, f)
+        f.write(unhexlify("0021")) # access_flags
+        write_class_data(data, pool, f)
+        #f.write(write_CONSTANT_Utf8("0"))
